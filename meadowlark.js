e6f9f1cc0fdfef55128103db35b62ed17c2e7f0c
Nikita Barinov <nikitabarinov2301@gmail.com>
2018-04-02 18:35:18 +0400

    added body-parser

--- meadowlark.js
+++ meadowlark.js
@@ -25,6 +25,7 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser').urlencoded({extended: true}));
 
 app.use(function(req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
